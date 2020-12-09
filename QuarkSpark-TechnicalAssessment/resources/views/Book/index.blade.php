dfc8f3ad0a70f5bafdec62dfa01bd5c5c6aaccd4
fanny-yolg <fannyyolg@gmail.com>
2020-12-09 18:56:21 +0700

    database setup

---
+++ QuarkSpark-TechnicalAssessment/resources/views/Book/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.main')
+
+@section('title', 'Book List')
+
+@section('container')
+<div class="container">
+<div class = "row">
+<div class = "col-10"> 
+    <h1 class="mt-3">Dafatar Buku</h1>
+     <table class = "table">
+     <thead class="table-dark">
+    <tr>
+        <th scope = "col">#</th>
+        <th scope = "col">Title</th>
+        <th scope = "col">Author</th>
+        <th scope = "col">Picture</th>
+        <th scope = "col">Action</th>
+     </tr>
+     </thead>
+     <tbody>
+     @foreach( $book as $bk )
+     <tr>
+        <th scope = "row">{{ $loop->iteration}}</th>
+        <td>{{ $bk->Title}}</td>
+        <td>{{ $bk->Author}}</td>
+        <td>{{ $bk->Picture}}</td>
+        <td>
+            <a href="" class="badge-success">borrow this book</a>
+        </td>
+        @endforeach
+    </tr>
+    </tbody>
+
+
+     </table>
+    
+    
+    </div>
+    </div>
+</div>
+@endsection
---
+++ QuarkSpark-TechnicalAssessment/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout/main')
+
+@section('title', 'Learn PHP')
+
+@section('container')
+<div class="container">
+<div class = "row">
+<div class = "col-10"> 
+    <h1 class="mt-3">Welcome to QS e-Library page!</h1>
+    </div>
+    </div>
+</div>
+@endsection
